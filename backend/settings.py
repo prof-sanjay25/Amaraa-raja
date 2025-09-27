015265a003e5821e73103662c10d30593497835a
prof-sanjay25 <ksanjay.mc21@gmail.com>
2025-09-27 16:12:18 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -157,3 +157,11 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 # ----------------------------------------
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret-key")
+
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+
+
